de4c2a523559b6f199d356bb2a6bad530bae3db2
GHarshitha115 <54620828+GHarshitha115@users.noreply.github.com>
2020-03-26 20:48:39 +0530

    Create matrix script.py

---
+++ matrix script.py
@@ -0,0 +1,15 @@
+import re
+m,n = map(int,input().split())
+matrix=[]
+for i in range(m):
+    matrix.append(input()) 
+string = ''
+for i in range(n):
+    for j in range(m):
+        string += matrix[j][i]
+#print(string)
+pattern = re.compile(r'(\w)(\W+)(\w)')
+#print(pattern)
+subs = pattern.sub(r'\1 \3',string)
+print(subs)
+
